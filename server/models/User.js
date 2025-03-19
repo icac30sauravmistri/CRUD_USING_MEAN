29c6d8c9e788751905e032ba6357ac9e6a251af9
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-19 23:02:27 +0530

    user model created

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model("User", userSchema);
