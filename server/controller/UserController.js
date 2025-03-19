b27dd94510ed5161addbf53bfb5c3f134cbba31e
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-19 23:06:40 +0530

    create user api in nodejs is done

---
+++ server/controller/UserController.js
@@ -0,0 +1,15 @@
+import User from "../models/User.js";
+
+export const create = async (requestAnimationFrame, res) => {
+    try {
+        const users = new User(req.body);
+        if (!users) {
+            return res.status(404).json({ message: "Oops! User data not found!" })
+        }
+
+        const saved_data = await users.save();
+        res.status(200).json(saved_data);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
