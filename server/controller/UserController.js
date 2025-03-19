993d51b73ea9e7feb1085613f29c02aff6cc64ea
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-19 23:56:02 +0530

    delete user api created

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -60,3 +60,20 @@ export const update = async (req, res) => {
         res.status(500).json({ error: error });
     }
 }
+
+export const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+
+        if (!userExist) {
+            return res.status(404).json({ message: "Oops! User data not found!" })
+        }
+
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ message: "Whoohoo! User data deleted successfully!" });
+
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { create, getAllUsers, getSingleUser, update } from "../controller/UserController.js";
+import { create, deleteUser, getAllUsers, getSingleUser, update } from "../controller/UserController.js";
 
 const route = express.Router();
 
@@ -7,5 +7,6 @@ route.post("/users/create", create);
 route.get("/users", getAllUsers);
 route.get("/users/:id", getSingleUser);
 route.put("/users/edit/:id", update);
+route.delete("/users/delete/:id", deleteUser);
 
 export default route;
