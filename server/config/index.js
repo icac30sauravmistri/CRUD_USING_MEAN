282aa47b16e5e9655b80f818c51dec2be30f6099
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-19 22:52:59 +0530

    default export env variables

--- server/config/index.js
+++ server/config/index.js
@@ -5,4 +5,4 @@ const { DB_CONNECT, PORT } = process.env;
 // const DB_CONNECT = process.env.DB_CONNECT;
 // const PORT = process.env.PORT;
 
-export { DB_CONNECT, PORT }
+export default { DB_CONNECT, PORT }
