8f6bd19796072acab1bc2168043dfd0a9f7d5e65
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-19 22:32:54 +0530

    basic package installation and applicaiton is started (init commit)

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const { DB_CONNECT, PORT } = process.env;
+// const DB_CONNECT = process.env.DB_CONNECT;
+// const PORT = process.env.PORT;
+
+export { DB_CONNECT, PORT }
---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { DB_CONNECT, PORT } from "./config/index.js";
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors())
+
+mongoose.connect(DB_CONNECT).then(() => {
+    console.log("DB Connected successfully!");
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT} port`);
+    })
+}).catch(error => console.log(error));
