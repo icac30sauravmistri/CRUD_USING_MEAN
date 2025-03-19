135f8cd4d16df3f53d90679c5408684d70bc8b72
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-20 01:40:05 +0530

    input values handling function is pending

--- client/src/components/createuser/Create.jsx
+++ client/src/components/createuser/Create.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./create.css"
 import { Link } from 'react-router-dom'
 
 const Create = () => {
+
+  const users = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: ""
+  }
+
+  const { user, setUser } = useState(users);
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+    console.log(user);
+
+  }
   return (
     <div className='create-user'>
       <Link to={"/users"}>Back</Link>
@@ -10,19 +26,19 @@ const Create = () => {
       <form className='create-user-form'>
         <div className="input-group">
           <label htmlFor="first_name">First Name</label>
-          <input type="text" id="fname" name="fname" autoComplete="off" placeholder="Enter first name" />
+          <input type="text" id="fname" name="fname" autoComplete="off" placeholder="Enter first name" onChange={inputHandler} />
         </div>
         <div className="input-group">
           <label htmlFor="last_name">Last Name</label>
-          <input type="text" id="lname" name="lname" autoComplete="off" placeholder="Enter last name" />
+          <input type="text" id="lname" name="lname" autoComplete="off" placeholder="Enter last name" onChange={inputHandler} />
         </div>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" autoComplete="off" placeholder="Enter email" />
+          <input type="email" id="email" name="email" autoComplete="off" placeholder="Enter email" onChange={inputHandler} />
         </div>
         <div className="input-group">
           <label htmlFor="password">Passowrd</label>
-          <input type="password" id="password" name="password" autoComplete="off" placeholder="Enter password" />
+          <input type="password" id="password" name="password" autoComplete="off" placeholder="Enter password" onChange={inputHandler} />
         </div>
         <div className="input-group">
           <button type="submit">Add User</button>
