8d6583741cf6f35663d60bb4d046c5212d9dcb48
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-20 22:24:17 +0530

    add users in react using node api is done

--- client/src/components/createuser/Create.jsx
+++ client/src/components/createuser/Create.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import "./create.css"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import toast from "react-hot-toast";
 
 const Create = () => {
 
@@ -9,28 +11,38 @@ const Create = () => {
     last_name: "",
     email: "",
     password: ""
-  }
+  };
 
-  const { user, setUser } = useState(users);
+  const [user, setUser] = useState(users);
+  const navigate = useNavigate();
 
   const inputHandler = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
-    console.log(user);
+    // console.log(user);
+  };
 
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8000/api/users/create", user)
+      .then((response) => {
+        toast.success(response.data.msg, { position: "top-right" })
+        navigate("/users")
+        console.log(response);
+      }).catch(error => console.log(error))
   }
   return (
-    <div className='create-user'>
+    <div className="create-user">
       <Link to={"/users"}>Back</Link>
       <h3>Add New User</h3>
-      <form className='create-user-form'>
+      <form className="create-user-form" onSubmit={submitForm}>
         <div className="input-group">
           <label htmlFor="first_name">First Name</label>
-          <input type="text" id="fname" name="fname" autoComplete="off" placeholder="Enter first name" onChange={inputHandler} />
+          <input type="text" id="fname" name="first_name" autoComplete="off" placeholder="Enter first name" onChange={inputHandler} />
         </div>
         <div className="input-group">
           <label htmlFor="last_name">Last Name</label>
-          <input type="text" id="lname" name="lname" autoComplete="off" placeholder="Enter last name" onChange={inputHandler} />
+          <input type="text" id="lname" name="last_name" autoComplete="off" placeholder="Enter last name" onChange={inputHandler} />
         </div>
         <div className="input-group">
           <label htmlFor="email">Email</label>
@@ -45,7 +57,7 @@ const Create = () => {
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
