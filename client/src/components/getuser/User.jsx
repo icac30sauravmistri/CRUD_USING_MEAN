4f586419436668b604d59dc34f232358f4aa8a1e
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-20 22:49:17 +0530

    get all user code in react using node api is done

--- client/src/components/getuser/User.jsx
+++ client/src/components/getuser/User.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./user.css";
+import axios from 'axios';
 
 const User = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("http://localhost:8000/api/users/")
+            setUsers(response.data);
+        }
+        fetchData();
+    }, [])
+
     return (
         <div className='user-table'>
             <Link to={"/users/create"} className='add-button'>Add User</Link>
@@ -16,15 +28,22 @@ const User = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>SM</td>
-                        <td>sm@sm.com</td>
-                        <td className='action-buttons'>
-                            <Link to={"/users/edit"}><i className="fa-solid fa-user-pen"></i></Link>
-                            <button><i className="fa-solid fa-trash-can"></i></button>
-                        </td>
-                    </tr>
+                    {
+                        users.map((user, index) => {
+                            return (
+                                <tr key={user._id}>
+                                    <td>{++index}</td>
+                                    <td>{user.first_name} {user.last_name}</td>
+                                    <td>{user.email}</td>
+                                    <td className='action-buttons'>
+                                        <Link to={"/users/edit"}><i className="fa-solid fa-user-pen"></i></Link>
+                                        <button><i className="fa-solid fa-trash-can"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+
                 </tbody>
             </table>
         </div>
